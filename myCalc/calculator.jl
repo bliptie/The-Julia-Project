6aa597b9e20ea2fc31dfdd688f00e65ca56a6be3
Julia Huijser <huijsejuli@ecs.vuw.ac.nz>
2023-12-23 12:58:51 +1300

    error resolved with commented instructions

--- myCalc/calculator.jl
+++ myCalc/calculator.jl
@@ -8,7 +8,7 @@ using Blink
 function myCalc()
     w = Window() # initialises the main Window
 
-    f = open("calculator.html") do file
+    f = open("calculator.html") do file # remember to change to this directory, e.g. right click file and select Julia: Change to this directory
         read(file, String)
     end
 
