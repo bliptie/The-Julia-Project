6f9e3cac622ac387b73a082fc239c2152f7497a4
Julia Huijser <huijsejuli@ecs.vuw.ac.nz>
2023-12-23 12:43:58 +1300

    finished myCalc function

--- myCalc/calculator.jl
+++ myCalc/calculator.jl
@@ -4,4 +4,11 @@ Blink.AtomShell.install() # downloads Blink dependencies
 function myCalc()
     w = Window() # initialises the main Window
 
-    f = open
+    f = open("calculator.html") do file
+        read(file, String)
+    end
+
+    body!(w,f) # part of the Blink.jl library, ! is a Julia convention meaning the function can modify its arguments
+end
+
+myCalc()
